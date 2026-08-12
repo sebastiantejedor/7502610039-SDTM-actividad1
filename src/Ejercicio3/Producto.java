26fe0c2451b033db83591b8aa0b83f458afe5bf5
Sebastian Tejedor <sebaspolar982@gmail.com>
2026-08-12 09:10:52 -0500

    feat(ejercicio3): agrego ejercicio 3

---
+++ src/Ejercicio3/Producto.java
@@ -0,0 +1,13 @@
+package Ejercicio3;
+
+public class Producto {
+    String nombre;
+    double precio;
+    long Stock;
+
+    void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: " + precio);
+        System.out.println("Stock: " + Stock);
+    }
+}
