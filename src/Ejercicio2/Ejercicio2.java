609f3d9ad5f49f44c783ce630bdb578ba9ab520a
Sebastian Tejedor <sebaspolar982@gmail.com>
2026-08-12 09:00:30 -0500

    feat(ejercicio2): mejoro ejercicio 2

--- src/Ejercicio2/Ejercicio2.java
+++ src/Ejercicio2/Ejercicio2.java
@@ -5,21 +5,13 @@ public class Ejercicio2 {
             IO.println("Ejercicio 2: Programacion Orientada a Objetos");
 
             Vehiculo vehiculo1 = new Vehiculo();
+
             vehiculo1.marca = "Lamborghini";
             vehiculo1.Modelo = "Aventador SVJ";
             vehiculo1.VelocidadActual = "350 km/h";
+
             vehiculo1.printVehiculoInfo();
         }
     }
-    public static class Vehiculo {
-        String marca;
-        String Modelo;
-        String VelocidadActual;
 
-        void printVehiculoInfo() {
-            IO.println("Marca: " + marca);
-            IO.println("Modelo: " + Modelo);
-            IO.println("Velocidad Actual: " + VelocidadActual);
-         }
-    }
 }
---
+++ src/Ejercicio2/Vehiculo.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+
+public class Vehiculo {
+        String marca;
+        String Modelo;
+        String VelocidadActual;
+
+        void printVehiculoInfo() {
+            IO.println("Marca: " + marca);
+            IO.println("Modelo: " + Modelo);
+            IO.println("Velocidad Actual: " + VelocidadActual);
+        }
+    }
+
