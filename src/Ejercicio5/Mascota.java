30d4a370defe904611dd05285626111cb951274b
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-16 18:22:01 -0500

    feat(ejercicio5): agrego ejercicio 5

---
+++ src/Ejercicio5/Mascota.java
@@ -0,0 +1,13 @@
+package Ejercicio5;
+
+public class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    void mostraMascota() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad);
+    }
+}
