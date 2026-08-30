7c9db988c68c2beef7580c3621974f85b15215e3
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-30 18:36:13 -0500

    feat(ejercicio32): agrego ejercicio 32

---
+++ src/Ejercicio32/Main.java
@@ -0,0 +1,15 @@
+package Ejercicio32;
+public class Main {
+    public static void main(String[] args) {
+
+                Producto producto1 = Producto.crearProductoBasico();
+                Producto producto2 = Producto.crearProductoBasico();
+
+                producto1.mostrarInformacion();
+                System.out.println();
+
+                producto2.mostrarInformacion();
+            }
+        }
+
+
