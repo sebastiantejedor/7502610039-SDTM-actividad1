ef48886442a9f450b065d7b1c8a25ede9441833d
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-30 18:20:42 -0500

    feat(ejercicio24): agrego ejercicio 24

---
+++ src/Ejercicio24/Libro.java
@@ -0,0 +1,13 @@
+package Ejercicio24;
+
+public class Libro {
+    public String titulo;
+    public String autor;
+    public int numPaginas;
+
+    public Libro(String titulo, String autor, int numPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+    }
+}
