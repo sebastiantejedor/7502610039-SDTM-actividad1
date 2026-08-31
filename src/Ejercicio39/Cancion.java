806a28af8c37df134874ef727f537c975733790f
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-31 09:38:11 -0500

    feat(ejercicio39): agrego ejercicio 39

---
+++ src/Ejercicio39/Cancion.java
@@ -0,0 +1,13 @@
+package Ejercicio39;
+
+public class Cancion {
+    String titulo;
+    String artista;
+    int duracion;
+
+    void mostrarInformacion(){
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Artista: " + artista);
+        System.out.println("Duracion en segundos: " + duracion);
+    }
+}
---
+++ src/Ejercicio39/Main.java
@@ -0,0 +1,13 @@
+package Ejercicio39;
+
+public class Main {
+    public static void main(String[] args) {
+        Cancion cancion1 = new Cancion();
+
+        cancion1.titulo = "APT";
+        cancion1.artista = "Rose & bruno Mars";
+        cancion1.duracion = 120;
+        cancion1.mostrarInformacion();
+
+    }
+}
