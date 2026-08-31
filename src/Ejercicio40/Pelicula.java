5f7c271d303e04e50cbc039efba43c2bc8e2f2be
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-31 09:38:38 -0500

    feat(ejercici40): agrego ejercicio 40

---
+++ src/Ejercicio40/Pelicula.java
@@ -0,0 +1,13 @@
+package Ejercicio40;
+
+public class Pelicula {
+    String nombre;
+    String genero;
+    int minutos;
+
+    void mostrarFicha() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Género: " + genero);
+        System.out.println("Duración: " + minutos + " minutos");
+    }
+}
