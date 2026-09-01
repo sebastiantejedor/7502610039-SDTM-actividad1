5d0c57f644e0b87e4e3e8f113e43fa1f3a76894e
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-31 19:18:18 -0500

    feat(ejercicio45): agrego ejercicio 45

---
+++ src/Ejercicio45/Persona.java
@@ -0,0 +1,21 @@
+package Ejercicio45;
+
+public class Persona {
+        String nombre;
+
+        public void saludar() {
+            System.out.println("Hola");
+        }
+}
+
+/*
+Codigo Errado:
+
+public class Persona {
+    String nombre;
+}
+
+public void saludar() {
+    System.out.println("Hola");
+}
+ */
