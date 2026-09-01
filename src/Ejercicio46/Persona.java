54af9d64f220fb673fc9ea57cd98b230a93db3e8
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-31 19:20:22 -0500

    feat(ejercicio46): agrego ejercicio 46

---
+++ src/Ejercicio46/Persona.java
@@ -0,0 +1,21 @@
+package Ejercicio46;
+
+public class Persona {
+    String nombre;
+
+    public void mostrarNombre() {
+        System.out.println(nombre);
+    }
+}
+
+/*
+Ejemplo errado
+
+public class {
+    String nombre;
+
+    public void mostrarNombre() {
+        System.out.println(nombre);
+    }
+}
+ */
