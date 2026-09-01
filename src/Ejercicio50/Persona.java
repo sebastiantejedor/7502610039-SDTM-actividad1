b4c336f2414561c8ec0237ebed50d771bd9bb34e
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-31 19:33:59 -0500

    feat(ejercicio50): agrego ejercicio 50

---
+++ src/Ejercicio50/Persona.java
@@ -0,0 +1,33 @@
+package Ejercicio50;
+
+public class Persona {
+    String nombre;
+
+    public void saludar() {
+        System.out.println("Hola " + nombre);
+    }
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.nombre = "Sebas";
+        persona.saludar();
+    }
+}
+/*
+Ejemplo errado:
+
+public class Persona {
+    String nombre;
+
+    public void saludar() {
+        System.out.println("Hola " + nombre);
+    }
+
+    public static void main(String[] args) {
+        Persona persona;
+
+        persona.saludar();
+    }
+}
+ */
